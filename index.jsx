a661f8b428224bdea50ca7e39ab482928e33f0a1
Shubham Kumar <100601869+shubhambanthu@users.noreply.github.com>
2023-05-22 12:04:06 +0530

    Add files via upload

--- index.jsx
+++ index.jsx
@@ -1,95 +1,77 @@
-import Icon from "@material-tailwind/react/Icon";
+import { Modal, ModalBody, ModalFooter } from "@material-tailwind/react";
 import Button from "@material-tailwind/react/Button";
-import pdfMake from "pdfmake/build/pdfmake";
-import pdfFonts from "pdfmake/build/vfs_fonts";
-// pdfMake.vfs = pdfFonts.pdfMake.vfs;
-import StateToPdfMake from "draft-js-export-pdfmake";
-import TextEditor from "../../components/TextEditor";
+import Icon from "@material-tailwind/react/Icon";
+import { Link } from "react-router-dom";
+import { useState, useContext } from "react";
+import { doc, deleteDoc } from "firebase/firestore";
+import { firestore } from "../../fireabase/config";
 import { AuthContext } from "../../context/firebase";
-import { useContext, useEffect } from "react";
-import { Link, useParams, useHistory } from "react-router-dom";
-import { signOut } from "@firebase/auth";
-import { auth, firestore } from "../../fireabase/config";
-import { doc, getDoc } from "@firebase/firestore";
-import { useState } from "react";
 
-const Editor = () => {
-  const { user, setUser } = useContext(AuthContext);
-  const [userDoc, setUserDoc] = useState(null);
-  const history = useHistory();
-  const { id } = useParams();
-  if (user === null) history.push("/");
-
-  useEffect(() => {
-    const getUerDoc = async () => {
-      const docRef = doc(
-        firestore,
-        "userDocs",
-        `${user?.uid}`,
-        "docs",
-        `${id}`
-      );
-      const docSnap = await getDoc(docRef);
-      if (docSnap.exists()) setUserDoc(docSnap.data());
-      else history.push("/");
-    };
-    getUerDoc();
-  }, [id, user?.uid, history]);
+const DowRow = ({ id, fileName, date }) => {
+  const [showModel, setShoModel] = useState(false);
+  const toggle = () => setShoModel(!showModel);
+  const { user } = useContext(AuthContext);
+  const deleteDocument = async (id) => {
+    // console.log(id);
+    try {
+      const docRef = doc(firestore, "userDocs", `${user?.uid}`, "docs", id);
+      console.log(docRef);
+      if (docRef) {
+        deleteDoc(docRef);
+        setShoModel(false);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <>
-      <header className="flex justify-between items-center p-3 pb-1">
-        <span className="cursor-pointer">
-          <Link to="/">
-            <Icon name="description" color="blue" size="5xl" />
-          </Link>
-        </span>
-        <div className="flex-grow px-2">
-          <h2 className="">{userDoc?.name}</h2>
-          <div className="flex items-center overflow-x-scroll text-sm space-x-1 ml-1 text-gray-600">
-            <p className="options">File</p>
-            <p className="options">Edit</p>
-            <p className="options">View</p>
-            <p className="options">Insert</p>
-            <p className="options">Format</p>
-            <p className="options">Tools</p>
-            <p className="options">Add-ons</p>
-            <p className="options">Help</p>
-          </div>
-        </div>
+      <div className="flex max-w-3xl mx-auto items-center p-4 rounded-lg hover:bg-gray-300 cursor-pointer text-gray-700 text-sm">
+        <Icon name="article" size="3xl" color="blue" />
+
+        <Link to={`/doc/${id}`} className="flex items-center w-full">
+          <p className="flex-grow pl-5 pr-10">{fileName}</p>
+          <p className="pr-5 text-sm truncate">{`${date
+            ?.toDate()
+            ?.toDateString("en-US")} at ${date
+            ?.toDate()
+            ?.toLocaleTimeString("en-US")}`}</p>
+        </Link>
         <Button
-          size="regular"
-          style={{ background: "#1A73E8" }}
-          className="!bg-[#1A73E8] hover:bg-blue-500 !rounded-md md:inline-flex h-10"
-          rounded={false}
-          block={false}
-          iconOnly={false}
-          ripple="light"
-          onClick={() => {
-            const stateToPdfMake = new StateToPdfMake(userDoc?.editorState);
-            // console.log(stateToPdfMake.generate());
-            pdfMake.vfs = pdfFonts.pdfMake.vfs;
-            pdfMake
-              .createPdf(stateToPdfMake.generate())
-              .download(`${userDoc?.name}.pdf`);
-          }}
+          color="gray"
+          buttonType="outline"
+          ripple="dark"
+          iconOnly={true}
+          rounded={true}
+          className="border-0 block"
+          onClick={() => setShoModel(true)}
         >
-          <Icon name="download" size="md" />
-          <span>Download</span>
+          <Icon name="delete" size="3xl" />
         </Button>
-        <img
-          src={user?.photoURL}
-          alt={user?.displayName}
-          title={user?.displayName}
-          onClick={() => {
-            signOut(auth);
-            setUser(null);
-          }}
-          className="cursor-pointer rounded-full h-10 w-10 ml-2"
-        />
-      </header>
-      <TextEditor uid={user?.uid} id={id} />
+      </div>
+      <Modal size="sm" active={showModel} toggler={toggle}>
+        <ModalBody>Do you really want to delete this document?</ModalBody>
+        <ModalFooter>
+          <Button
+            color="red"
+            onClick={() => deleteDocument(id)}
+            ripple="dark"
+            type="submit"
+          >
+            Yes
+          </Button>
+          <Button
+            color="blue"
+            buttonType="link"
+            onClick={() => setShoModel(false)}
+            ripple="dark"
+          >
+            No
+          </Button>
+        </ModalFooter>
+      </Modal>
     </>
   );
 };
 
-export default Editor;
+export default DowRow;
