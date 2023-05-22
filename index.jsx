0066688638c5b7c183a8f1a5f320f8acd69cbab1
Shubham Kumar <100601869+shubhambanthu@users.noreply.github.com>
2023-05-22 12:00:10 +0530

    Add files via upload

--- index.jsx
+++ index.jsx
@@ -1,72 +1,158 @@
-import React, { useContext } from "react";
+// importing components
+import Header from "../../components/Header";
+import DocRow from "../../components/DocRow/";
 
+// importing material ui
 import Button from "@material-tailwind/react/Button";
+import Icon from "@material-tailwind/react/Icon";
+import Modal from "@material-tailwind/react/Modal";
+import ModalFooter from "@material-tailwind/react/ModalFooter";
+import ModalBody from "@material-tailwind/react/ModalBody";
+import { useContext, useEffect, useState } from "react";
 
-import { useHistory } from "react-router-dom";
-
+// Firestore
+import { firestore } from "../../fireabase/config";
 import {
-  GoogleAuthProvider,
-  onAuthStateChanged,
-  signInWithPopup,
-} from "firebase/auth";
-import { auth, firestore } from "../../fireabase/config";
-import { setDoc, serverTimestamp, doc } from "firebase/firestore";
-
-// importing firebase context
+  addDoc,
+//   getDocs,
+  collection,
+  serverTimestamp,
+//   query,
+//   orderBy,
+  onSnapshot,
+} from "firebase/firestore";
+import { useHistory } from "react-router";
 import { AuthContext } from "../../context/firebase";
 
-const Login = () => {
-  //get the user state from the context
-  const { setUser } = useContext(AuthContext);
+const Home = () => {
+  const [showModel, setShoModel] = useState(false);
+  const [input, setInput] = useState("");
+  const [userDoc, setUserDoc] = useState([]);
   const history = useHistory();
-  onAuthStateChanged(auth, (user) => {
-    if (user) history.push("/");
-  });
 
-  const login = () => {
-    const provider = new GoogleAuthProvider();
+  const { user } = useContext(AuthContext);
 
-    signInWithPopup(auth, provider)
-      .then(({ user }) => {
-        const docRef = doc(firestore, "users", `${user?.uid}`);
-        setDoc(
-          docRef,
-          {
-            lastLogin: serverTimestamp(),
-            name: user?.displayName,
-            email: user?.email,
-            number: user?.phoneNumber,
-          },
-          { merge: true },
-          (doc) => console.log(doc)
-        );
-        console.log(user);
-        setUser(user);
-      })
-      .catch((err) => console.log(err));
+  if (user === null) history.push("/login");
+  const createDoc = async () => {
+    if (!input) return;
+    setInput("");
+    setShoModel(false);
+
+    const docRef = await addDoc(
+      collection(firestore, "userDocs", `${user?.uid}`, "docs"),
+      {
+        name: `${input}`,
+        time: serverTimestamp(),
+      }
+    );
+    history.push(`/doc/${docRef?.id}`);
   };
+  const model = (
+    <Modal size="sm" active={showModel} toggler={() => setShoModel(false)}>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          createDoc();
+        }}
+      >
+        <ModalBody>
+          <input
+            type="text"
+            className="outline-none w-full bg-gray-200  p-3 rounded-md"
+            placeholder="Enter name of the document."
+            onChange={({ target }) => setInput(target.value)}
+            value={input}
+          />
+        </ModalBody>
+        <ModalFooter>
+          <Button
+            color="blue"
+            buttonType="link"
+            onClick={() => setShoModel(false)}
+            ripple="dark"
+          >
+            Cancle
+          </Button>
+          <Button color="blue" onClick={createDoc} ripple="dark" type="submit">
+            Create
+          </Button>
+        </ModalFooter>
+      </form>
+    </Modal>
+  );
 
+  useEffect(() => {
+    const unsub = onSnapshot(
+      collection(firestore, "userDocs", `${user?.uid}`, "docs"),
+      (snap) => {
+        setUserDoc(
+          snap.docs?.map((doc) => ({
+            id: doc?.id,
+            ...doc.data(),
+          }))
+        );
+      }
+    );
+    return () => unsub();
+  }, [user?.uid]);
   return (
     <>
-      <div className="flex flex-col items-center justify-center h-100 min-h-screen">
-        <img
-          src="https://links.papareact.com/1ui"
-          height="300"
-          width="550"
-          alt="logo"
-        />
-        <Button
-          className="w-44 mt-10 "
-          color="blue"
-          buttonType="filled"
-          ripple="light"
-          onClick={login}
-        >
-          Login
-        </Button>
-      </div>
+      <Header />
+      {model}
+      <section
+        style={{ background: "#f8f9fa" }}
+        className="bg-[#f8f9fa] p-2 w-full mx-auto md:pb-10 md:px-10 "
+      >
+        <div className="max-w-3xl w-full mx-auto">
+          <div className="py-6 flex items-center justify-between">
+            <h2 className="text-gray-700">Start a new document</h2>
+            <Button
+              color="gray"
+              ripple="dark"
+              buttonType="outline"
+              iconOnly={true}
+              className="border-0"
+            >
+              <Icon name="more_vert" size="3xl" />
+            </Button>
+          </div>
+          <div>
+            <div
+              className="relative h-52 w-40 border-2 cursor-pointer hover:border-blue-700"
+              onClick={() => setShoModel(true)}
+            >
+              <img src="https://links.papareact.com/pju" alt="add-doc" />
+            </div>
+            <p className="ml-2 mt-2 font-semibold text-sm text-gray-700">
+              Blank
+            </p>
+          </div>
+        </div>
+      </section>
+      <section className="bg-white w-full md:px-10 md:mb-20">
+        <div className="max-w-3xl mx-auto py-8 text-sm text-gray-700">
+          <div className="flex p-4 items-center justify-between">
+            <h2 className="font-medium flex-grow">My Document</h2>
+            <p className="mr-12">Date Created</p>
+            <Icon name="folder" size="3xl" color="gray" />
+          </div>
+        </div>
+        {userDoc.length === 0 ? (
+          <div className="w-full text-center py-5">No documents</div>
+        ) : (
+          ""
+        )}
+        {userDoc?.map((doc) => (
+          <DocRow
+            id={doc?.id}
+            key={doc?.id}
+            fileName={doc?.name}
+            date={doc?.time}
+          />
+        ))}
+      </section>
     </>
   );
 };
 
-export default Login;
+export default Home;
